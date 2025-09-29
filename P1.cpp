e31816958f3901dbb8133dc15b7778649456437f
Varnika-06 <varnikastuff234@gmail.com>
2025-09-29 20:18:10 +0530

    Create P1.cpp

---
+++ P1.cpp
@@ -0,0 +1,32 @@
+//Name: Varnika Maurya
+//PRN: 24070123160
+//Exp No. 16
+//Program No.: 1
+#include <iostream>
+using namespace std;
+int main()
+{
+    float n1, n2, ans;
+    cout << "Enter value of number 1 & 2: ";
+    cin >> n1 >> n2;
+    try 
+    {
+        if (n2 == 0) 
+            throw n2;
+        else 
+        {
+            ans = n1 / n2;
+            cout << "Answer = " << ans << "\n";
+        }
+    }
+    catch(float num)
+    {
+        cout << "\nERROR : division by " << num << "\n";
+    }
+}
+
+/* 
+Output:
+Enter value of number 1 & 2: 12 35
+Answer = 0.342857
+*/
